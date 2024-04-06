554757b08a4bbafd9b2d5aab1f894b1d27ada024
GurkanGUR <2gur1kan@gmail.com>
2024-04-06 16:17:10 +0300

    Add the project

---
+++ Archive/SaveForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace Archive
+{
+    partial class SaveForm
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Save = new System.Windows.Forms.Button();
+            this.Picture = new System.Windows.Forms.PictureBox();
+            this.Title = new System.Windows.Forms.TextBox();
+            this.Info = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Save
+            // 
+            this.Save.AutoSize = true;
+            this.Save.BackColor = System.Drawing.Color.Goldenrod;
+            this.Save.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.Save.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Save.Location = new System.Drawing.Point(0, 467);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(888, 45);
+            this.Save.TabIndex = 0;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = false;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Picture
+            // 
+            this.Picture.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Picture.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.Picture.Location = new System.Drawing.Point(532, 72);
+            this.Picture.Name = "Picture";
+            this.Picture.Size = new System.Drawing.Size(344, 227);
+            this.Picture.TabIndex = 3;
+            this.Picture.TabStop = false;
+            this.Picture.Click += new System.EventHandler(this.Picture_Click);
+            // 
+            // Title
+            // 
+            this.Title.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Title.Location = new System.Drawing.Point(0, 0);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(888, 44);
+            this.Title.TabIndex = 0;
+            this.Title.TextChanged += new System.EventHandler(this.Title_TextChanged);
+            // 
+            // Info
+            // 
+            this.Info.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Info.Location = new System.Drawing.Point(27, 72);
+            this.Info.Name = "Info";
+            this.Info.Size = new System.Drawing.Size(465, 31);
+            this.Info.TabIndex = 4;
+            this.Info.TextChanged += new System.EventHandler(this.Info_TextChanged);
+            // 
+            // SaveForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(888, 512);
+            this.Controls.Add(this.Info);
+            this.Controls.Add(this.Title);
+            this.Controls.Add(this.Picture);
+            this.Controls.Add(this.Save);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Name = "SaveForm";
+            this.Text = "SavePanel";
+            this.Load += new System.EventHandler(this.SaveForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Picture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.PictureBox Picture;
+        private System.Windows.Forms.TextBox Title;
+        private System.Windows.Forms.TextBox Info;
+    }
+}
+
---
+++ Archive/SaveForm.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Archive
+{
+    public partial class SaveForm : Form
+    {
+        public SaveForm()
+        {
+            InitializeComponent();
+        }
+
+        // Dosya adını belirle ve dosya yolunu oluştur
+        string archivePath = "Archive\\";
+        string dataPath = "Data\\";
+        public string titleDataBaseName = "Titles.txt";
+        string titlePath;
+
+        private void SaveForm_Load(object sender, EventArgs e)
+        {
+            //dosya adına sahip bir kolasör vermı ona bakar
+            titlePath = Path.Combine(Environment.CurrentDirectory, archivePath + titleDataBaseName);
+
+            try
+            {
+                // Dosya zaten varsa üzerine yazar, yoksa oluşturur
+                if (!File.Exists(titlePath))
+                {
+                    // Dosya yoksa oluştur
+                    using (FileStream fs = File.Create(titlePath)) { }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Hata durumunda hata mesajını göster
+                MessageBox.Show("Hata: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            try // başlığı sisteme kaydeder (sonradan arama yparken kolaylık olsun diye)
+            {
+                if (!string.IsNullOrWhiteSpace(Title.Text) && Title.Text.Length >= 3) // veri 3 karkaterden fazla karaktere sahip olmalı
+                {
+                    if (!IsThereATitle(Title.Text)) //veri dosyada varmı kontrol et
+                    {
+                        // StreamWriter nesnesi oluştur ve dosyayı aç
+                        using (StreamWriter sw = new StreamWriter(titlePath, true))
+                        {
+                            // Veriyi dosyaya yaz
+                            sw.WriteLine(Title.Text);
+                        }
+
+                        MessageBox.Show("Başlık başarıyla kaydedildi.", "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Bulunan başlık!! Lütfen yeni bir başlık giriniz", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Lütfen en az 3 karakter içeren bir metin girin.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+
+                try // kalan veriyi kaydeden kısım 
+                {
+                    string DataPath = Path.Combine(Environment.CurrentDirectory, archivePath + dataPath + Title.Text + ".txt");
+
+                    // StreamWriter nesnesi oluştur ve dosyayı aç
+                    using (StreamWriter sw = new StreamWriter(DataPath, true))
+                    {
+                        // Veriyi dosyaya yaz
+                        sw.WriteLine(Title.Text);
+                        sw.WriteLine(Info.Text);
+                    }
+
+                    MessageBox.Show("Veri başarıyla kaydedildi.", "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    // Hata durumunda hata mesajını göster
+                    MessageBox.Show("Hata: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                // Hata durumunda hata mesajını göster
+                MessageBox.Show("Hata: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+           
+        }
+
+        /// <summary>
+        /// başlık varmı yokmu onu kontrol eder
+        /// </summary>
+        /// <param name="arananMetin"></param>
+        /// <returns></returns>
+        private bool IsThereATitle(string arananMetin)
+        {
+            // Dosyanın içeriğini satır satır oku
+            using (StreamReader sr = new StreamReader(titlePath))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    // Aranan metni içeren bir satır bulunursa true döndür
+                    if (line.Contains(arananMetin))
+                    {
+                        return true;
+                    }
+                }
+            }
+            // Aranan metin dosyada bulunamazsa false döndür
+            return false;
+        }
+
+        private void Picture_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Title_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Info_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
